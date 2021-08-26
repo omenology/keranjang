291e35ebe66224f519265feb9769141008ff8cc9
omenology <ikballukmanulhakim5@gmail.com>
2021-08-26 20:00:34 +0700

    form validation & loginwarper

---
+++ src/app/cart/components/LoginWraper.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import css from "../styles/login.module.css";
+
+const LoginWraper = (props) => {
+  return (
+    <div className={`${css.bgContainer} container-fluid`}>
+      <div className="row justify-content-md-center d-flex align-items-center" style={{ height: "100vh" }}>
+        <div className="col-md-5">
+          <div className={`${css.bgCard} card`} style={{ border: 0 }}>
+            {props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginWraper;
--- src/app/cart/pages/login.tsx
+++ src/app/cart/pages/login.tsx
@@ -1,45 +1,92 @@
 import React from "react";
-import { FloatingLabel, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { FloatingLabel, Button, Form } from "react-bootstrap";
+import LoginWraper from "../components/LoginWraper";
 
 import css from "../styles/login.module.css";
 
 const Login = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: { identifier: string; password: string }): void => {
+    console.log({
+      ...isEmail(data.identifier),
+      password: data.password,
+    });
+  };
+
+  const isEmail = (str: string): object => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(str)) {
+      return { email: str };
+    }
+    return { username: str };
+  };
+
+  const errorMessagePassword = (key: string): string => {
+    switch (key) {
+      case "minLength":
+        return "Password to short";
+      case "maxLength":
+        return "Password to long";
+      case "required":
+        return "Password is required";
+    }
+  };
+
   return (
-    <div className={`${css.bgContainer} container-fluid`}>
-      <div className="row justify-content-md-center d-flex align-items-center" style={{ height: "100vh" }}>
-        <div className="col-md-5">
-          <div className={`${css.bgCard} card`} style={{ border: 0 }}>
-            <h3 className="text-center mb-4" style={{ color: "ghostwhite" }}>
-              ACCOUNT LOGIN
-            </h3>
-            <form>
-              <div className="d-inline-flex" style={{ width: "100%" }}>
-                <FloatingLabel label="Email address" className={`${css.label} flex-grow-1`}>
-                  <input placeholder="name@example.com" type="email" className={`${css.ifocus} form-control`} style={{ borderRadius: "15px 0 0 0" }} />
-                </FloatingLabel>
-                <FloatingLabel label="Password" className={`${css.label} flex-grow-1`}>
-                  <input type="password" className={`${css.ifocus} form-control`} placeholder="lala" style={{ borderRadius: "0 15px 0 0" }} />
-                </FloatingLabel>
-              </div>
-              <div className="d-grid gap-2">
-                <Button type="submit" size="lg" variant="dark" style={{ borderRadius: "0 0 15px 15px" }}>
-                  SIGN IN
-                </Button>
-              </div>
-            </form>
-            <p className="text-center mt-3" style={{ color: "ghostwhite" }}>
-              <span onClick={() => console.log("register")} style={{ cursor: "pointer" }}>
-                Register
-              </span>{" "}
-              |{" "}
-              <span onClick={() => console.log("forget")} style={{ cursor: "pointer" }}>
-                Forget Password?
-              </span>
-            </p>
-          </div>
+    <LoginWraper>
+      <h3 className="text-center mb-4" style={{ color: "ghostwhite" }}>
+        ACCOUNT LOGIN
+      </h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="d-inline-flex" style={{ width: "100%" }}>
+          <FloatingLabel label="Username or Email" className={`${css.label} flex-grow-1`}>
+            <Form.Control
+              type="text"
+              {...register("identifier", { required: true })}
+              isInvalid={errors?.identifier}
+              placeholder="username or email"
+              className={`${css.ifocus} form-control`}
+              style={{ borderRadius: "15px 0 0 0" }}
+            />
+            <Form.Control.Feedback type="invalid" className={`${css.errorMsg}`}>
+              Username or Email is required
+            </Form.Control.Feedback>
+          </FloatingLabel>
+          <FloatingLabel label="Password" className={`${css.label} flex-grow-1`}>
+            <Form.Control
+              type="password"
+              {...register("password", { required: true, minLength: 4, maxLength: 16 })}
+              isInvalid={errors?.password}
+              className={`${css.ifocus} form-control`}
+              placeholder="password"
+              style={{ borderRadius: "0 15px 0 0" }}
+            />
+            <Form.Control.Feedback type="invalid" className={`${css.errorMsg}`}>
+              {errorMessagePassword(errors?.password?.type)}
+            </Form.Control.Feedback>
+          </FloatingLabel>
         </div>
-      </div>
-    </div>
+        <div className="d-grid gap-2">
+          <Button type="submit" size="lg" variant="dark" style={{ borderRadius: "0 0 15px 15px" }}>
+            SIGN IN
+          </Button>
+        </div>
+      </form>
+      <p className="text-center mt-3" style={{ color: "ghostwhite" }}>
+        <span onClick={() => console.log("register")} className={`${css.textBtn}`}>
+          Register
+        </span>{" "}
+        |{" "}
+        <span onClick={() => console.log("forget")} className={`${css.textBtn}`}>
+          Forget Password?
+        </span>
+      </p>
+    </LoginWraper>
   );
 };
 
